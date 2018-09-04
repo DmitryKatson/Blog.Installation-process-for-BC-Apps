e5ce315535cc7d77b9e6c77c4a070407121f65f5
Dmitry Katson <dmitry.katson@gmail.com>
2018-09-04 09:08:54 +0100

    sql query

---
+++ 06-CheckWhatIsInsideSqlTable.sql
@@ -0,0 +1,14 @@
+use [FinancialsCA] 
+Select * from [AIR Check install process$a00d5718-f6e5-4b48-994f-24823f1af879] where [Primary Key] > 12
+Select * from [CRONUS Canada, Inc_$Customer]
+SELECT TABLE_NAME FROM [FinancialsCA].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' 
+Select * from Company
+Select * from [NAV App]
+SELECT * from [NAV App Dependencies]
+SELECT * from [NAV App Object Prerequisites]
+Select * from [NAV App Capabilities]
+Select * from [NAV App Object Metadata]
+Select * from [NAV App Tenant App]
+Select * from [NAV App Data Archive]
+Select * from [NAV App Installed App]
+
